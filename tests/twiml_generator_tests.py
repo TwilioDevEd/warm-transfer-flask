c83b929837753118313cf0e6dce3804ca4f8aa4a
Victor Shyba <victor.shyba@gmail.com>
2016-05-02 12:18:46 -0300

    Adds a function to generate conference responses
    This function will help on building TwiML responses for conferences.

--- tests/twiml_generator_tests.py
+++ tests/twiml_generator_tests.py
@@ -4,11 +4,34 @@ from warm_transfer_flask import twiml_generator
 
 class RootTest(BaseTest):
 
-    def test_wait_conference(self):
+    def test_wait_conference_contains_verb_say_with_message(self):
         response = twiml_generator.generate_wait()
 
         root = self.assertXmlDocument(bytes(bytearray(response, encoding='utf-8')))
         wait_message = 'Thank you for calling. Please wait in line for a few seconds. An agent will be with you shortly.'
         self.assertEquals([wait_message], root.xpath('./Say/text()'))
+
+    def test_wait_conference_plays_music(self):
+        response = twiml_generator.generate_wait()
+
+        root = self.assertXmlDocument(bytes(bytearray(response, encoding='utf-8')))
         wait_music = 'http://com.twilio.music.classical.s3.amazonaws.com/BusyStrings.mp3'
         self.assertEquals([wait_music], root.xpath('./Play/text()'))
+
+    def test_generate_connect_conference_xml_conference_nested_on_dial(self):
+        response = twiml_generator.generate_connect_conference('', '', True, False)
+
+        root = self.assertXmlDocument(bytes(bytearray(response, encoding='utf-8')))
+        self.assertTrue(root.xpath('/Response/Dial/Conference'), response)
+
+    def test_generate_connect_conference_uses_parameters_for_attribs(self):
+        call_sid = 'CallSID'
+        url = 'http://www.example.com'
+
+        response = twiml_generator.generate_connect_conference(call_sid, url, True, False)
+
+        root = self.assertXmlDocument(bytes(bytearray(response, encoding='utf-8')))
+        conference = root.xpath('//Conference')[0]
+        self.assertEquals('true', conference.attrib['startConferenceOnEnter'])
+        self.assertEquals('false', conference.attrib['endConferenceOnExit'])
+        self.assertEquals(url, conference.attrib['waitUrl'])
--- warm_transfer_flask/twiml_generator.py
+++ warm_transfer_flask/twiml_generator.py
@@ -8,3 +8,11 @@ def generate_wait():
     twiml_response.say(wait_message)
     twiml_response.play(wait_music)
     return str(twiml_response)
+
+def generate_connect_conference(call_sid, wait_url, start_on_enter, end_on_exit):
+    twiml_response = twiml.Response()
+    dial = twiml_response.dial()
+    dial.conference(call_sid, startConferenceOnEnter=start_on_enter,
+                    endConferenceOnExit=end_on_exit,
+                    waitUrl=wait_url)
+    return str(twiml_response)
