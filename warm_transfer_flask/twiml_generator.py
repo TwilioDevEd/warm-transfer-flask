0553a17aee8f6cbde296392a13efb77c6dea0ae0
Victor Shyba <victor.shyba@gmail.com>
2016-05-02 17:27:48 -0300

    fix pep8
    Adding a missing line, should have 2

--- warm_transfer_flask/twiml_generator.py
+++ warm_transfer_flask/twiml_generator.py
@@ -9,6 +9,7 @@ def generate_wait():
     twiml_response.play(wait_music)
     return str(twiml_response)
 
+
 def generate_connect_conference(call_sid, wait_url, start_on_enter, end_on_exit):
     twiml_response = twiml.Response()
     dial = twiml_response.dial()
