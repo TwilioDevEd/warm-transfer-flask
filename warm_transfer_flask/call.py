d50a497fb976ee64f93495d4d5496ea3a970cc71
mosampaio <mosampaio@gmail.com>
2016-09-26 18:17:20 -0300

    Bumps twilio sdk version to 6.0.0rc12

--- warm_transfer_flask/call.py
+++ warm_transfer_flask/call.py
@@ -1,4 +1,4 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 from os import environ as ENV
 
 
@@ -6,7 +6,7 @@ def call_agent(agent_id, callback_url):
     account_sid = ENV['TWILIO_ACCOUNT_SID']
     auth_token = ENV['TWILIO_AUTH_TOKEN']
     my_number = ENV['TWILIO_NUMBER']
-    client = TwilioRestClient(account_sid, auth_token)
+    client = Client(account_sid, auth_token)
     call = client.calls.create(to='client:' + agent_id, from_=my_number,
                                url=callback_url)
     return call.sid
